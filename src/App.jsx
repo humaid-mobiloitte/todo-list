c6e363404a6c11643d47eba4018609fdb6af892f
humaid.ansari <humaid.ansari@mobiloitte.com>
2025-02-01 19:00:26 +0530

    made some advancement in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Card, CardContent, Button, Container, Box, CssBaseline } from '@mui/material';
+import { AppBar, Toolbar, Typography, Card, CardContent, Button, Container, Box, CssBaseline,TextField } from '@mui/material';
 
 function App() {
   return (
@@ -8,20 +8,18 @@ function App() {
       {/* Navbar */}
       <AppBar position="sticky">
         <Toolbar>
-          <Typography variant="h6">My MUI App</Typography>
+          <Typography variant="h6">My TODO List App</Typography>
         </Toolbar>
       </AppBar>
 
       {/* Main Content */}
       <Container maxWidth="sm" sx={{ flex: 1, mt: 4 }}>
         <Card sx={{ backgroundColor: '#f5f5f5', boxShadow: 3 }}>
-          <CardContent>
+          <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems:'center',gap:'15px'}}>
             <Typography variant="h5" sx={{ mb: 2 }}>
-              Welcome to MUI
+              TO-DO List
             </Typography>
-            <Typography variant="body1" sx={{ mb: 2 }}>
-              This is a simple card with basic Material UI components.
-            </Typography>
+            <TextField id="filled-basic" label="Enter your task" variant="filled" />
             <Button variant="contained" color="primary">Click Me</Button>
           </CardContent>
         </Card>
@@ -29,7 +27,7 @@ function App() {
 
       {/* Footer */}
       <Box sx={{ bgcolor: '#1976d2', color: 'white', p: 2, textAlign: 'center' }}>
-        <Typography variant="body2">© 2025 My MUI App</Typography>
+        <Typography variant="body2">© 2025 My TODO List App</Typography>
       </Box>
     </div>
   );
